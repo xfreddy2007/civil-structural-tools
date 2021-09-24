34a6ba07eb0ff3202c0ae1c64a259a37de5678e4
xfreddy2007 <xfreddy2007@gmail.com>
2021-09-24 10:26:32 +0800

    feat: add index seo meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,13 @@ export default function Home() {
   return (
     <div className="flex-1">
       <Head>
-        <title>Civil Structural Calculation Tools</title>
+        <title key="title">Civil Structural Calculation Tools</title>
+        <link rel="canonical" href="https://civil-structural-tools.herokuapp.com/"/>
+        <meta property="og:title" content="Civil Structural Calculation Tools" />
+        <meta property="og:url" content="https://civil-structural-tools.herokuapp.com/" />
+        <meta property="og:image" content="/assets/png/homepage-smo.png" />
         <meta name="description" content="Tools for Civil Engineers who need calculation for designs" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Main />
       <Author />
