373f33d9cd6233573a6860cbaa777383ddbca856
xfreddy2007 <xfreddy2007@gmail.com>
2021-09-24 09:49:06 +0800

    feat: delete lazyloadCallback

--- libs/utils/lazyloadCallback.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const lazyloadCallback= (ref:React.MutableRefObject<any>):void => {
-  ref.current.classList.add('is-image-loaded')
-};
-
-export default lazyloadCallback;
